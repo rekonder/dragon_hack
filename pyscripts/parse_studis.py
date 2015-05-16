3ab5e17b0948eafd73def391b37ce622b4411b12
Tine <subic.tine@gmail.com>
2015-05-16 15:19:10 +0200

    Initial Py script

---
+++ pyscripts/parse_studis.py
@@ -0,0 +1,34 @@
+__author__ = 'Tine'
+
+from bs4 import BeautifulSoup
+import json
+
+filename = "..\pyresources\studis.html"
+myfile = open(filename,encoding="utf-8")
+unicode_html = myfile.read()
+
+soup = BeautifulSoup(unicode_html)
+OUT = open("../pyresources/out.txt","w")
+student = {}
+tmp = soup.title.contents[0].split(" ")
+
+student["name"] = tmp[0]
+student["surname"] = tmp[1]
+
+json.dump(student,OUT)
+
+program = soup.find_all("div", "izbraniVpis")[0]
+student["program"] = program.find_all("p")[0].contents[0].strip()
+print(student)
+OUT.close()
+
+
+
+
+
+
+
+
+
+
+
