c7fd93b0df743ddfe825faa9808746f056f271ab
Tine <subic.tine@gmail.com>
2015-05-16 21:11:11 +0200

    final APS parse

--- pyscripts/parse_ucilnica.py
+++ pyscripts/parse_ucilnica.py
@@ -3,21 +3,42 @@ __author__ = 'Tine'
 import requests
 from bs4 import BeautifulSoup
 import json
+import re
 
 aps2_link = "https://ucilnica.fri.uni-lj.si/course/view.php?id=309"
 
 site = "https://ucilnica.fri.uni-lj.si/login/index.php"
 payload = {"username": "ts7328@student.uni-lj.si", "password":"Speedwolf8"}
 
+
+def cleanText(text):
+       alpha = "čČšŠćĆđĐžŽ"
+       repl = "cCsScCdDzZ"
+       for i in range(0,len(alpha)):
+               text = text.replace(alpha[i],repl[i],100)
+       return text
+
+
 def getApsData(pk):
         html = requests.get("https://ucilnica.fri.uni-lj.si/mod/page/view.php?id=32848", cookies=pk)
-        aps2 = {}
-        aps2["link"] = aps2_link
+        aps2 = {"link": aps2_link}
 
-        soup = BeautifulSoup(html.text.encode("utf-8"))
-        data = soup.find_all('div')
-        print(data)
+        txt = cleanText(html.text)
 
+        soup = BeautifulSoup(txt)
+        data = soup.find_all('a',"urlextern")
+
+        aps2["predavateljLink"] = data[1]["href"]
+        aps2["predavatelj"] = data[1].contents[0]
+
+        data = soup.find_all('a', href=True)
+        mails = []
+        for d in data:
+            if "mailto" in d["href"]:
+                mails.append(d["href"][7:])
+        aps2["predavateljMail"] = mails.pop(0)
+
+        aps2["asistenti"] = ",".join(mails)
 
 
         return aps2
