eef79cc1e1db7a2df64dba0ffd0ea5100d1bdc6f
MikroMan <subic.tine@gmail.com>
2015-05-19 15:29:43 +0200

    Added naloge.py to general run script

--- fri_scrapers/naloge.py
+++ fri_scrapers/naloge.py
@@ -3,15 +3,13 @@ __author__ = 'MikroMan'
 import requests
 from bs4 import BeautifulSoup
 import json
+import sys
+from fri_scrapers import color_out as c_out
 
 koledar = "https://ucilnica.fri.uni-lj.si/my/index.php"
 
 site = "https://ucilnica.fri.uni-lj.si/login/index.php"
-payload = {"username": "ts7328@student.uni-lj.si", "password": "Casomerilci@20"}
-
-with requests.Session() as v:
-    p = v.post(site, data=payload)  # saying our credentials to the site
-    pk = v.cookies  # saving cookies as "pk"
+file_name = './public/data/naloge.json'
 
 
 def oddano(param):
@@ -21,7 +19,7 @@ def oddano(param):
         return "ne"
 
 
-def parseRok(param):
+def parse_due_date(param):
     t = param.split(" ")
     t.pop(0)
     t[1] += ","
@@ -30,7 +28,7 @@ def parseRok(param):
     return t
 
 
-def parseNaloga(span, cookie):
+def parse_assignment_data(span, cookie):
     data = {"link": span}
     html = requests.get(span, cookies=cookie)
     soup = BeautifulSoup(html.text)
@@ -45,12 +43,12 @@ def parseNaloga(span, cookie):
     table = table.find_all('td', 'cell')
 
     data["oddano"] = oddano(table[1].contents[0])
-    data["rok"] = parseRok(table[5].contents[0])
+    data["rok"] = parse_due_date(table[5].contents[0])
 
     return data
 
 
-def parseKviz(span, cookie):
+def parse_quiz_data(span, cookie):
     data = {"link": span}
     html = requests.get(span, cookies=cookie)
     soup = BeautifulSoup(html.text)
@@ -69,7 +67,7 @@ def parseKviz(span, cookie):
     return data
 
 
-def getEventData(cookie):
+def get_event_data(cookie):
     html = requests.get(koledar, cookies=cookie)
 
     soup = BeautifulSoup(html.text)
@@ -81,17 +79,30 @@ def getEventData(cookie):
         span = event.find_all('img')[0]
         if span['title'] == "Naloga":
             span = event.find_all('a')[0]['href']
-            events['naloge'].append(parseNaloga(span, cookie))
+            events['naloge'].append(parse_assignment_data(span, cookie))
 
         elif span["title"] == "Kviz":
             span = event.find_all('a')[0]['href']
-            events["kvizi"].append(parseKviz(span, cookie))
+            events["kvizi"].append(parse_quiz_data(span, cookie))
 
     return events
 
 
-data = getEventData(pk)
+def main(user, password):
+    print("Scraping assignemtns...", end='')
+    payload = {"username": user, "password": password}
 
-file = open("../public/data/naloge.json", "w", encoding="utf-8")
-json.dump(data, file, ensure_ascii=False)
-file.close()
+    with requests.Session() as v:
+        p = v.post(site, data=payload)  # saying our credentials to the site
+        pk = v.cookies  # saving cookies as "pk"
+
+    data = get_event_data(pk)
+
+    file = open(file_name, "w", encoding="utf-8")
+    json.dump(data, file, ensure_ascii=False)
+    file.close()
+    c_out.print_color(c_out.OKGREEN, '[OK]')
+
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])
--- run_parsers.py
+++ run_parsers.py
@@ -1,24 +1,13 @@
 __author__ = 'MikroMan'
 
 import sys
-from fri_scrapers import urnik, studis
+from fri_scrapers import urnik, studis, naloge
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-    def printColor(color,msg,end_line='\n'):
-        print(color + msg + bcolors.ENDC, end=end_line)
 
 def main(user, password):
     result = urnik.main()
     result = studis.main()
+    result = naloge.main(user, password)
 
 
 if __name__ == '__main__':
