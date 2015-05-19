7dd8a320bda90d6169bd0ca5d13d8632ecd0640d
MikroMan <subic.tine@gmail.com>
2015-05-19 20:05:02 +0200

    Added and cleaned parse_ucilnica for general script but currently not in use.

--- fri_scrapers/parse_ucilnica.py
+++ fri_scrapers/parse_ucilnica.py
@@ -1,56 +1,61 @@
+import sys
+
 __author__ = 'MikroMan'
 
 import requests
 from bs4 import BeautifulSoup
 import json
+from fri_scrapers import color_out as c_out
+
 aps2_link = "https://ucilnica.fri.uni-lj.si/course/view.php?id=309"
-
 site = "https://ucilnica.fri.uni-lj.si/login/index.php"
-payload = {"username": "ts7328@student.uni-lj.si", "password":"Speedwolf8"}
+aps_link_subpage = "https://ucilnica.fri.uni-lj.si/mod/page/view.php?id=32848"
 
 
-def cleanText(text):
-       alpha = "čČšŠćĆđĐžŽ"
-       repl = "cCsScCdDzZ"
-       for i in range(0,len(alpha)):
-               text = text.replace(alpha[i],repl[i],100)
-       return text
+def clean_text(text):
+    alpha = "čČšŠćĆđĐžŽ"
+    repl = "cCsScCdDzZ"
+    for i in range(0, len(alpha)):
+        text = text.replace(alpha[i], repl[i], 100)
+    return text
 
 
-def getApsData(pk):
-        html = requests.get("https://ucilnica.fri.uni-lj.si/mod/page/view.php?id=32848", cookies=pk)
-        aps2 = {"link": aps2_link}
+def get_aps_data(pk):
+    html = requests.get(aps_link_subpage, cookies=pk)
+    aps2 = {"link": aps2_link}
 
-        txt = cleanText(html.text)
+    txt = clean_text(html.text)
 
-        soup = BeautifulSoup(txt)
-        data = soup.find_all('a',"urlextern")
+    soup = BeautifulSoup(txt)
+    data = soup.find_all('a', "urlextern")
 
-        aps2["predavateljLink"] = data[1]["href"]
-        aps2["predavatelj"] = data[1].contents[0]
+    aps2["predavateljLink"] = data[1]["href"]
+    aps2["predavatelj"] = data[1].contents[0]
 
-        data = soup.find_all('a', href=True)
-        mails = []
-        for d in data:
-            if "mailto" in d["href"]:
-                mails.append(d["href"][7:])
-        aps2["predavateljMail"] = mails.pop(0)
+    data = soup.find_all('a', href=True)
+    mails = []
+    for d in data:
+        if "mailto" in d["href"]:
+            mails.append(d["href"][7:])
+    aps2["predavateljMail"] = mails.pop(0)
 
-        aps2["asistenti"] = ",".join(mails)
+    aps2["asistenti"] = ",".join(mails)
 
-        return aps2
-
-
-with requests.Session() as v:
-        p = v.post(site, data=payload) ## saying our credentials to the site
-        pk=v.cookies  ## saving cookies as "pk"
+    return aps2
 
 
+def main(user, passwd):
+    print("Scraping course data from Ucilnica... ", end='')
+    with requests.Session() as v:
+        response = v.post(site, data={'username': user, 'password': passwd})  # saying our credentials to the site
+        cookie = v.cookies  # saving cookies as "pk"
+    aps_data = get_aps_data(cookie)
+    file = open("./public/data/ucilnica.json", "w", encoding="utf-8")
+    json.dump(aps_data, file)
+
+    file.close()
+    c_out.print_color(c_out.OKGREEN, '[OK]')
 
 
-aps_data = getApsData(pk)
-file = open("../pyresources/ucilnica.json","w",encoding="utf-8")
-json.dump(aps_data,file)
-
-file.close()
-
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])
--- run_parsers.py
+++ run_parsers.py
@@ -1,13 +1,16 @@
 __author__ = 'MikroMan'
 
 import sys
-from fri_scrapers import urnik, studis, naloge
+from fri_scrapers import urnik, studis, naloge, parse_ucilnica
 
 
 def main(user, password):
     result = urnik.main()
     result = studis.main()
     result = naloge.main(user, password)
+
+    #parse_ucilnica not used due to currently not providing implemented info
+    #result = parse_ucilnica.main(user,password)
 
 
 if __name__ == '__main__':
