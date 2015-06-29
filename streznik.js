ff19c91583bbfbbbfd31afa3c8030f2910b2930e
MikroMan <subic.tine@gmail.com>
2015-06-29 21:56:46 +0200

    English'ed server file.

--- streznik.js
+++ streznik.js
@@ -1,59 +1,57 @@
-//Konec razvoja naloge 2.8 + konec popravkov
+
 var http = require('http');
 var fs  = require('fs');
 var path = require('path');
 var mime = require('mime');
-var predpomnilnik = {};
+var cache = {};
 
-function posredujNapako404(odgovor) {
-  odgovor.writeHead(404, {'Content-Type': 'text/plain'});
-  odgovor.write('Napaka 404: Vira ni mogoče najti.');
-  odgovor.end();
+function forwardError404(response) {
+  response.writeHead(404, {'Content-Type': 'text/plain'});
+  response.write('Error 404: Source not found.');
+  response.end();
 }
 
-function posredujDatoteko(odgovor, datotekaPot, datotekaVsebina) {
-  odgovor.writeHead(200, {"content-type": mime.lookup(path.basename(datotekaPot))});
-  odgovor.end(datotekaVsebina);
+function forwardFile(response, filePath, fileContent) {
+  response.writeHead(200, {"content-type": mime.lookup(path.basename(filePath))});
+  response.end(fileContent);
 }
 
-function posredujStaticnoVsebino(odgovor, predpomnilnik, absolutnaPotDoDatoteke) {
-  if (predpomnilnik[absolutnaPotDoDatoteke]) {
-    posredujDatoteko(odgovor, absolutnaPotDoDatoteke, predpomnilnik[absolutnaPotDoDatoteke]);
+function forwardStaticContent(response, cache, absoluteFilePath) {
+  if (cache[absoluteFilePath]) {
+    forwardFile(response, absoluteFilePath, cache[absoluteFilePath]);
   } else {
-    fs.exists(absolutnaPotDoDatoteke, function(datotekaObstaja) {
-      if (datotekaObstaja) {
-        fs.readFile(absolutnaPotDoDatoteke, function(napaka, datotekaVsebina) {
+    fs.exists(absoluteFilePath, function(fileExists) {
+      if (fileExists) {
+        fs.readFile(absoluteFilePath, function(napaka, datotekaVsebina) {
           if (napaka) {
-            posredujNapako404(odgovor);
+            forwardError404(response);
           } else {
-            predpomnilnik[absolutnaPotDoDatoteke] = datotekaVsebina;
-            posredujDatoteko(odgovor, absolutnaPotDoDatoteke, datotekaVsebina);
+            cache[absoluteFilePath] = datotekaVsebina;
+            forwardFile(response, absoluteFilePath, datotekaVsebina);
           }
         });
       } else {
-        posredujNapako404(odgovor);
+        forwardError404(response);
       }
     });
   }
 }
 
-var streznik = http.createServer(function(zahteva, odgovor) {
-  var potDoDatoteke = false;
+var streznik = http.createServer(function(req, resp) {
+  var filePath = false;
 
-  if (zahteva.url == '/') {
-    potDoDatoteke = 'public/index.html';
-  } else {
-    potDoDatoteke = 'public' + zahteva.url;
-  }
+  if (req.url == '/')
+    filePath = 'public/index.html';
+  else
+    filePath = 'public' + req.url;
 
-  var absolutnaPotDoDatoteke = './' + potDoDatoteke;
-  posredujStaticnoVsebino(odgovor, predpomnilnik, absolutnaPotDoDatoteke);
+  var absoluteFilePath = './' + filePath;
+  forwardStaticContent(resp, cache, absoluteFilePath);
 });
 
 streznik.listen(3000, function() {
-  console.log("Strežnik posluša na portu " +  3000 + ".");
+  console.log("Server listening on port " +  3000 + ".");
 });
-
 
 var fbStreznik = require('./lib/fbapi_streznik.js');
 fbStreznik.listen(streznik);
