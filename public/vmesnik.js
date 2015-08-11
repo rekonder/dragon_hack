14fec724b910393939a7d32fea5a95994c134e34
zigacernigoj <ziga.cernigoj@gmail.com>
2015-08-11 17:39:07 +0200

    english-ifed the code in vmesnik.js

--- public/vmesnik.js
+++ public/vmesnik.js
@@ -7,7 +7,7 @@ String.prototype.capitalizeFirst = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 };
 
-var kraticeGlobal = [];
+var allSubjects = [];
 var hiddenSubjects = [];
 /* ---------- ZACETEK ---------- ZA KUKIJE ---------- */
 
@@ -58,10 +58,12 @@ function checkCookie() {
     }
 }
 
+/*
 function changeSubjects(){
 	// zaenkrat samo v alertu pokaze vpisno + vse predmete (skupaj v arrayu)
 	checkCookie();
 }
+*/
 
 function checkCout() {
 	var userData=getCookie("hideSubjects");
@@ -122,6 +124,7 @@ function hideBlocksC(idToHide){
 	$(idToHide).hide();
 }
 
+/*
 function setSubFrame(subV, x){
 	$("#vsebinaPredmet" + x).append(
 		'<div class="panel panel-default">' + 
@@ -135,6 +138,7 @@ function setSubFrame(subV, x){
 		'</div>'
 	);
 }
+*/
 
 function setSubFrame2(subVs, x){
 	for(var i in subVs){
@@ -171,12 +175,12 @@ function setFrame(x){
 }
 
 $.getJSON( "data/studis.json", function( data ) {
-	var predmeti = data.predmeti;
+	var subjects = data.predmeti;
 	var i = 0;
-	for(var key in predmeti){
-		kraticeGlobal[i] = key;
+	for(var key in subjects){
+		allSubjects[i] = key;
 		setFrame(key);
-		fillPredmet(predmeti, key);
+		fillSubject(subjects, key);
 
 		fillChangeSubjects(key);
 		hiddenSubjects[i] = 'frame' + key + ":" + false;
@@ -184,27 +188,25 @@ $.getJSON( "data/studis.json", function( data ) {
 		i++;
 	}
 
-	//changeSubjects();
-
 	var student = data.student;
-	var ime = student.name;
-	var priimek = student.surname;
-	$("#imeStudent").append('<a href="javascript:alert(\'prijavljeni ste kot: ' + ime + ' ' + priimek + '\')">' + ime + " " + priimek + '</a>');
-	var program = student.program;
-	$("#program").append('<a href="javascript:alert(\'program: ' + program + '\')">' + program + '</a>');
+	var stName = student.name;
+	var stSurname = student.surname;
+	$("#imeStudent").append('<a href="javascript:alert(\'prijavljeni ste kot: ' + stName + ' ' + stSurname + '\')">' + stName + " " + stSurname + '</a>');
+	var stProgram = student.program;
+	$("#program").append('<a href="javascript:alert(\'program: ' + stProgram + '\')">' + stProgram + '</a>');
 
 });
 
-function fillPredmet(podatki, x){
-	var imeP = podatki[x].name;
-	$("#imePredmet" + x).append("<b><a href='javascript:hide(vsebinaPredmet"+x+")'>"+ x + " | " + imeP+"</a></b>");
+function fillSubject(data, x){
+	var subjName = data[x].name;
+	$("#imePredmet" + x).append("<b><a href='javascript:hide(vsebinaPredmet"+x+")'>"+ x + " | " + subjName+"</a></b>");
 
 	$("#izpiti" + x).append("<table class='table table-hover'> <tr id='stev" + x + "'> <tr id='roki" + x + "'> ");
-	var datumi = podatki[x].dates.split(",");
-	for(var i in datumi){
-		if(datumi[i] !== ""){
+	var examDates = data[x].dates.split(",");
+	for(var i in examDates){
+		if(examDates[i] !== ""){
 			$("#stev" + x).append('<th>' + (parseInt(i)+1) +'.rok</th>');
-			$("#roki" + x).append('<td>' + datumi[i] +'</td>');
+			$("#roki" + x).append('<td>' + examDates[i] +'</td>');
 		}
 	}
 }
@@ -215,154 +217,154 @@ function fillChangeSubjects(x){
 
 
 $.getJSON( "data/urnik.json", function( data ) {
-	var predavanja = data.predavanja;
-	var vaje = data.vaje;
+	var lectures = data.predavanja;
+	var practice = data.vaje;
 	//console.log(predavanja + " " + vaje );
 
-	for(var key in predavanja){
-		fillPredavanja(predavanja, key);
+	for(var key in lectures){
+		fillLectures(lectures, key);
 	}
-	for(var key in vaje){
-		fillVaje(vaje, key);
+	for(var key in practice){
+		fillPractice(practice, key);
 	}
 });
 
-function fillPredavanja(pred, x){
-	var predX = pred[x];
+function fillLectures(lectures, x){
+	var lectureX = lectures[x];
 	//console.log(predX);
 	$("#predavanja" + x).append("<table id=predavanjaTabela" + x + " class='table table-hover'>");
 
-	for (var key in predX){
+	for (var key in lectureX){
 		//console.log(predX[key].ucilnica + " " + predX[key].termin + " " + predX[key].predavatelj);
 		$("#predavanjaTabela" + x).append("<tr>" +
-			"<td>" + predX[key].ucilnica + "</td>" + 
-			"<td>" + predX[key].termin + "</td>" + 
-			"<td>" + predX[key].predavatelj + "</td>" + 
+			"<td>" + lectureX[key].ucilnica + "</td>" +
+			"<td>" + lectureX[key].termin + "</td>" +
+			"<td>" + lectureX[key].predavatelj + "</td>" +
 			"</tr>"
 		);
 	}
 
 	$("#izvajalci" + x).append("<table id='izvajalciTabela" + x + "' class='table table-hover'>");
-	$("#izvajalciTabela" + x).append('<tr id=predavatelj' + x + '> <th>Predavatelj</th> <td><a href="">' + predX[0].predavatelj + '</a></td> <td><button type="button" class="btn btn-default" aria-label="Left Align"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span></button></td> </tr>');
+	$("#izvajalciTabela" + x).append('<tr id=predavatelj' + x + '> <th>Predavatelj</th> <td><a href="">' + lectureX[0].predavatelj + '</a></td> <td><button type="button" class="btn btn-default" aria-label="Left Align"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span></button></td> </tr>');
 }
 
-function fillVaje(vaje, x){
-	var vajeX = vaje[x];
+function fillPractice(practice, x){
+	var practiceX = practice[x];
 	//console.log(vajeX);
 	$("#vaje" + x).append("<table id=vajeTabela" + x + " class='table table-hover'>");
 	
 	var key;
-	var asistentArr = [], idx=0;
-	for (key in vajeX){
+	var assistantArr = [], idx=0;
+	for (key in practiceX){
 		$("#vajeTabela" + x).append("<tr>" +
-			"<td>" + vajeX[key].ucilnica + "</td>" + 
-			"<td>" + vajeX[key].termin + "</td>" + 
-			"<td>" + vajeX[key].asistent + "</td>" + 
+			"<td>" + practiceX[key].ucilnica + "</td>" +
+			"<td>" + practiceX[key].termin + "</td>" +
+			"<td>" + practiceX[key].asistent + "</td>" +
 			"</tr>"
 		);
 
-		if(asistentArr.indexOf(vajeX[key].asistent)<0){
-			asistentArr[idx] = vajeX[key].asistent;
+		if(assistantArr.indexOf(practiceX[key].asistent)<0){
+			assistantArr[idx] = practiceX[key].asistent;
 			idx++;
-			$("#izvajalciTabela" + x).append('<tr id="asistent' + x + idx + '"> <th>Asistent</th> <td>' + asistentArr[idx-1] +'</td> <td><button type="button" class="btn btn-default" aria-label="Left Align"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span></button></td> </tr>');
+			$("#izvajalciTabela" + x).append('<tr id="asistent' + x + idx + '"> <th>Asistent</th> <td>' + assistantArr[idx-1] +'</td> <td><button type="button" class="btn btn-default" aria-label="Left Align"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span></button></td> </tr>');
 		}
 	}
 }
 
 $.getJSON( "data/naloge.json", function( data ) {
 	//console.log(data);
-	//console.log(kraticeGlobal);
+	//console.log(allSubjects);
 
-	var kvizi = data.kvizi;
-	var naloge = data.naloge;
+	var quizzes = data.kvizi;
+	var tasks = data.naloge;
 
-	var poPredmKvizi = [];
-	for (var i = kraticeGlobal.length - 1; i >= 0; i--) {
-		poPredmKvizi[i]=[];
+	var quizzesSubjects = [];
+	for (var i = allSubjects.length - 1; i >= 0; i--) {
+		quizzesSubjects[i]=[];
 	}
-	for(var key1 in kraticeGlobal){
+	for(var key1 in allSubjects){
 		var i = 0;
-		for(var key2 in kvizi){
-			if(kvizi[key2].name === kraticeGlobal[key1]){
-				poPredmKvizi[key1][i] = kvizi[key2];
+		for(var key2 in quizzes){
+			if(quizzes[key2].name === allSubjects[key1]){
+				quizzesSubjects[key1][i] = quizzes[key2];
 				i++;
 			}
 		}
 	}
 	//console.log(poPredmKvizi);
 
-	var poPredmNaloge = [];
-	for (var i = kraticeGlobal.length - 1; i >= 0; i--) {
-		poPredmNaloge[i] = [];
+	var tasksSubjects = [];
+	for (var i = allSubjects.length - 1; i >= 0; i--) {
+		tasksSubjects[i] = [];
 	}
 
-	for(var key1 in kraticeGlobal){
+	for(var key1 in allSubjects){
 		var i = 0;
-		for(var key2 in naloge){
-			if(naloge[key2].name === kraticeGlobal[key1]){
-				poPredmNaloge[key1][i] = naloge[key2];
+		for(var key2 in tasks){
+			if(tasks[key2].name === allSubjects[key1]){
+				tasksSubjects[key1][i] = tasks[key2];
 				i++;
 			}
 		}
 	}
 	//console.log(poPredmNaloge);
 
-	for(var key in poPredmKvizi){
-		fillKvizi(poPredmKvizi, key);
+	for(var key in quizzesSubjects){
+		fillQuizzes(quizzesSubjects, key);
 	}
-	for(var key in poPredmNaloge){
-		fillNaloge(poPredmNaloge, key);
+	for(var key in tasksSubjects){
+		fillTasks(tasksSubjects, key);
 	}
 });
 
 
-function fillKvizi(kvizi, x){
-	var kviziX = kvizi[x];
+function fillQuizzes(quizzes, x){
+	var quizzesX = quizzes[x];
 
-	if(typeof kviziX[0] !== "undefined"){
-		//console.log("fillKvizi" + kviziX[0].name);
-		$("#obveznosti" + kviziX[0].name).append("<table id=kviziTabela" + kviziX[0].name + " class='table table-hover'>");
+	if(typeof quizzesX[0] !== "undefined"){
+		//console.log("fillQuizzes" + kviziX[0].name);
+		$("#obveznosti" + quizzesX[0].name).append("<table id=kviziTabela" + quizzesX[0].name + " class='table table-hover'>");
 
-		for(var key in kviziX){
-			$("#kviziTabela" + kviziX[0].name).append("<tr>" +
+		for(var key in quizzesX){
+			$("#kviziTabela" + quizzesX[0].name).append("<tr>" +
 				"<td>kviz</td>" + 
-				"<td><a href='" + kviziX[key].link+"' target='_blank'>" + kviziX[key].rok + "</td>" +
+				"<td><a href='" + quizzesX[key].link+"' target='_blank'>" + quizzesX[key].rok + "</td>" +
 				"</tr>"
 			);
 		}
 	}
 	else{
-		$("#obveznosti" + kraticeGlobal[x]).append("Ni kvizov</br>");
+		$("#obveznosti" + allSubjects[x]).append("Ni kvizov</br>");
 		// slucajno dela, ni pa zagotovo, da bo delalo v vseh primerih
 	}
 }
 
-function fillNaloge(nal, x){
-	var nalX = nal[x];
+function fillTasks(tasks, x){
+	var tasksX = tasks[x];
 
-	if(typeof nalX[0] !== "undefined"){
-		//console.log("fillNaloge" + nalX[0].name);
-		$("#obveznosti" + nalX[0].name).append("<table id=nalogeTabela" + nalX[0].name + " class='table table-hover'>");
+	if(typeof tasksX[0] !== "undefined"){
+		//console.log("fillTasks" + nalX[0].name);
+		$("#obveznosti" + tasksX[0].name).append("<table id=nalogeTabela" + tasksX[0].name + " class='table table-hover'>");
 		
 		var key;
-		for(key in nalX){
-			if(nalX[key].oddano === "da"){
+		for(key in tasksX){
+			if(tasksX[key].oddano === "da"){
 				var s = '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>'
 			}
 			else{
 				var s = '<span class="glyphicon glyphicon-flag" aria-hidden="true"></span>'
 			}
 
-			$("#nalogeTabela" + nalX[0].name).append("<tr>" +
+			$("#nalogeTabela" + tasksX[0].name).append("<tr>" +
 				"<td>naloga</td>" + 
-				"<td><a href='" + nalX[key].link + "' target='_blank'>" + nalX[key].rok + "</td>" +
+				"<td><a href='" + tasksX[key].link + "' target='_blank'>" + tasksX[key].rok + "</td>" +
 				"<td>"+s+"</td>" +
 				"</tr>"
 			);
 		}
 	}
 	else{
-		$("#obveznosti" + kraticeGlobal[x]).append("Ni nalog</br>");
+		$("#obveznosti" + allSubjects[x]).append("Ni nalog</br>");
 		// slucajno dela, ni pa zagotovo, da bo delalo v vseh primerih
 	}
 }
